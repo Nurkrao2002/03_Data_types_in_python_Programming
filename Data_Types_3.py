667cf26820d83d9495bfe5dd170311d4babe0284
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+uadaynalabothu@users.noreply.github.com>
2022-08-25 19:36:17 +0530

    Update Data_Types_3.py

--- Data_Types_3.py
+++ Data_Types_3.py
@@ -1,19 +1,19 @@
-from numbers import Complex
+# from numbers import Complex
 
 
-print(type('hello'))            # String
-print(type(b'hello'))           # bytes
-print(type(r'hello\new'))       # String
-a='python'
-print(f'hello {a}')             
-print(type(2+3j))               # Complex
-print(type(4j))                 # Complex
+# print(type('hello'))            # String
+# print(type(b'hello'))           # bytes
+# print(type(r'hello\new'))       # String
+# a='python'
+# print(f'hello {a}')             
+# print(type(2+3j))               # Complex
+# print(type(4j))                 # Complex
 
-a=6
-print(float(a))
-print('%f' %a)
-print('%.4f' %a)
-b=0.2 + 0.4
-print('%f'%b)
-print('%.1f'%b)
-print('%.2f'%b)
+# a=6
+# print(float(a))
+# print('%f' %a)
+# print('%.4f' %a)
+# b=0.2 + 0.4
+# print('%f'%b)
+# print('%.1f'%b)
+# print('%.2f'%b)
